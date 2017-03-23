b999925f1081354b8ecb79a5c3e6808936eeb233
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-03-23 02:56:32 -0300

    Updated sample query to fetch relations.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import { graphql } from 'graphql'
 import { schema } from './schema'
 
-const query = `{
-  post(id: 1) {
-    id
-    title
-    body
+const query = `
+  {
+    post (id: 1) {
+      id
+      author {
+        id
+        posts {
+          id
+          title
+          body
+        }
+      }
+    }
   }
-}`
+`
 
 // Prints
 // {
-//   data: { post: { id: 1, body: '...', title: '...' } }
+//   "data": {
+//     "post": {
+//       "id": 1,
+//       "author": {
+//         "id": 1,
+//         "posts": [...]
+//       }
+//     }
+//   }
 // }
 graphql(schema, query).then(console.log)
